0acdb21096f4293ffe90bbc0bc6f7cb192582d77
unknown <ashubackup>
2024-05-10 17:55:43 +0530

    Add Id in TblAirtime

--- src/main/java/com/Entity/TblAirtime.java
+++ src/main/java/com/Entity/TblAirtime.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -19,6 +22,8 @@ import lombok.NoArgsConstructor;
 public class TblAirtime 
 {
 
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
 	private Integer id;
 	private String msisdn;
 	private String gameId;
